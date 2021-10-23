f142978328befb45f589f36c584da0c4b47b25c0
gnoul_ <ttluong4869@gmail.com>
2021-10-23 20:14:35 +0700

    Complete the project

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React, { useEffect } from 'react';
+import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
-  return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
-}
+    const { setSearchTerm } = useGlobalContext();
+    const searchValue = React.useRef('');
 
-export default SearchForm
+    const searchCocktail = () => {
+        setSearchTerm(searchValue.current.value);
+    };
+
+    useEffect(() => {
+        searchValue.current.focus();
+    }, []);
+
+    return (
+        <section className="section search">
+            <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+                <div className="form-control">
+                    <label htmlFor="name">Search your favorite cocktail</label>
+                    <input
+                        type="text"
+                        name=""
+                        id="name"
+                        ref={searchValue}
+                        onChange={searchCocktail}
+                    />
+                </div>
+            </form>
+        </section>
+    );
+};
+
+export default SearchForm;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">About us</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+                blanditiis libero repellendus laboriosam minus excepturi! At
+                officiis, ut enim cupiditate, odit doloribus natus delectus nemo
+                laboriosam, molestias beatae minus soluta totam consequatur
+                vitae ratione. Repellendus eum alias nihil culpa quasi corporis
+                dolores possimus dolorum recusandae quia, ducimus dicta
+                temporibus officia, maiores inventore vero atque illum
+                asperiores esse? Quam, nemo tenetur?
+            </p>
+        </section>
+    );
+};
 
-export default About
+export default About;
